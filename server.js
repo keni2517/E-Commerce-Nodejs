a19e6607f9db652492e81c829791f017a8071f9d
keni <kenikakadiya@gmail.com>
2024-09-13 23:19:24 +0530

    server

--- server.js
+++ server.js
@@ -12,42 +12,42 @@ const path = require('path');
 server.use(morgan("dev"));
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-server.use("/public/images",express.static(path.join(__dirname,"public/images")))
+server.use("/public/images", express.static(path.join(__dirname, "public/images")))
 
-server.get('/',(req,res)=>{
-    res.send({msg:"welcome to Express Page"});
+server.get('/', (req, res) => {
+    res.send({ msg: "welcome to Express Page" });
 })
 
 
 //User Routes
 const userRoutes = require('./routes/user.routes');
-server.use("/api/user",userRoutes);
+server.use("/api/user", userRoutes);
 
 
 //OTP Routes
 const otpRoutes = require('./routes/otp.routes');
-server.use("/api/otp",otpRoutes);
+server.use("/api/otp", otpRoutes);
 
 //Product Routes
 const productRoutes = require('./routes/product.routes');
-server.use("/api/product",productRoutes);
+server.use("/api/product", productRoutes);
 
 //Cart Routes
 const cartRoutes = require('./routes/cart.routes');
-server.use("/api/cart",cartRoutes);
+server.use("/api/cart", cartRoutes);
 
 
 //order Routes
-const orderRoutes= require('./routes/order.routes');
-server.use("/api/order",orderRoutes);
+const orderRoutes = require('./routes/order.routes');
+server.use("/api/order", orderRoutes);
 
 //Review Routes
-const reviewRoutes=require('./routes/review.routes');
-server.use("/api/review",reviewRoutes)
+const reviewRoutes = require('./routes/review.routes');
+server.use("/api/review", reviewRoutes)
 
 //wishlist Routes
-const wishlistRoutes=require('./routes/wishlist.routes')
-server.use("/api/wishlist",wishlistRoutes)
+const wishlistRoutes = require('./routes/wishlist.routes')
+server.use("/api/wishlist", wishlistRoutes)
 
 server.listen(ports, () => {
     // Database connection
