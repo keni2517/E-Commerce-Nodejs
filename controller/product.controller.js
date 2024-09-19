f42402c660024d45e9e373d6ce897d59bce07eed
keni <kenikakadiya@gmail.com>
2024-09-19 08:28:25 +0530

    poduct

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,15 +1,15 @@
-// product add   /// connect with database
-
+const ProductService= require('../services/product.services');
+const productService = new ProductService();
 const Product = require("../model/product.model");
 
 exports.addNewProduct = async (req,res) =>{
   try{
     // console.log(req.body);
     const {productName , price , discription , rating , othersProducts ,image} = req.body;
-    let product = await Product.findOne({productName :productName});
+    let product = await productService.findProduct({productName :productName});
     if(product)
       return res.status(400).json({message : "Product Already Exist.."});
-    product = await Product.create({
+    product = await productService. addProduct({
       productName ,  price , discription , rating , othersProducts ,image
     });
     res.status(201).json({product , message : "Product added"})
@@ -53,7 +53,7 @@ exports.updateProduct = async (req ,res) =>{
       return res.status(404).json({message : "Product Not Found...."})
     }
     // product = await Product.updateOne({_id:req.query.prodcutId} , {$set :req.body} , {new : true})
-    product = await Product.findByIdAndUpdate(req.query.productId, {$set :req.body} , {new : true});
+    product = await productService.updateProduct(req.query.productId, {$set :req.body} , {new : true});
     product.save();
     res.status(202).json({product , message : "Product Update Success"});
   }catch(error){
@@ -71,7 +71,7 @@ exports.deleteProduct = async (req ,res) =>{
       return res.status(404).json({message : "Product Not Found...."})
     }
     // product = await Product.deleteOne({_id : product._id});
-    product = await Product.findByIdAndDelete(product._id);
+    product = await productService.deleteProduct(product._id);
     res.status(200).json({product  , message : "Product Delete Success"})
   }catch(error){
     console.log(error);
